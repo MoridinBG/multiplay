55ad29101ae198ef3db46a63725296410d126a54
root.darkstar <root.darkstar@1bd3bf80-89e4-11de-b317-bf1d2adb44e4>
2009-09-19 22:36:09 +0000

    - Added correct version of TextRenderer.mm

--- trunk/TextRender.mm
+++ trunk/TextRender.mm
@@ -15,26 +15,19 @@
 {
 	if(self = [super init])
 	{
-		GLint ctx, myFont;
 		strings = [[NSMutableArray alloc] init];
+
+/*		[strings addObject:@"Здравей, приятелю! "];
+		[strings addObject:@"Наслади се на питието си! "]; */
 		
 		[strings addObject:@"Enjoy a good drink! "];
 		[strings addObject:@"How is your evening? "];
 		[strings addObject:@"Did you have a great day today? "];
-		[strings addObject:@"Random test string from me. "];
-		[strings addObject:@"What you gonna do? "];
+		[strings addObject:@"What you gonna do tonight? "];
 		
-		ctx = glcGenContext();
-		glcContext(ctx);
-		glcAppendCatalog("/Users/ivandilchovski/Fonts/");
-		
-		glcRenderStyle(GLC_TRIANGLE);
-		glcStringType(GLC_UTF8_QSO);
-		glcEnable(GLC_GL_OBJECTS);
-		
-		myFont = glcGenFontID();
-		glcNewFontFromFamily(myFont, "Courier New");
-		glcFont(myFont);
+		font = new FTGLPolygonFont("/Users/ivandilchovski/Fonts/Courier.ttf");
+		font->FaceSize(30);
+		font->UseDisplayList(true);
 	}
 	
 	return self;
@@ -47,18 +40,21 @@
 	if([event ignoreEvent])
 		return;
 	
-	NSNumber *uniqueID = event.uid;
+	NSNumber *uniqueID = event.uid;	
 	
-	CGPoint oldPos = event.lastPos;
+//	CGPoint oldPos = event.lastPos;
 	CGPoint pos = event.pos;
+	
+	pos.x *= 640;
+	pos.y *= 640;
 		
 	switch (event.type) 
 	{
 		case TouchDown:
 		{
 			[Logger logMessage:@"Processing TextRender touch down event" ofType:DEBUG_TOUCH];
-			InteractiveObject *text = [[InteractiveObject alloc] initWithPos:pos];
-			[text setLabel:[strings objectAtIndex:(arc4random() % [strings count])]];
+			LabeledInteractor *text = [[LabeledInteractor alloc] initWithPos:pos];
+			text.label = [strings objectAtIndex:(arc4random() % [strings count])];
 			
 			[touches setObject:text forKey:uniqueID];
 		} break;
@@ -66,7 +62,7 @@
 		{
 			[Logger logMessage:@"Processing TextRender touch move event" ofType:DEBUG_TOUCH_MOVE];
 			
-			[(InteractiveObject*)[touches objectForKey:uniqueID] setPosition:pos];
+			[(LabeledInteractor*)[touches objectForKey:uniqueID] setPosition:pos];
 		} break;
 		case TouchRelease:
 		{
@@ -77,52 +73,60 @@
 }
 
 - (void) render
-{
+{	
 	float circumference, diameter;
-	InteractiveObject *text;
+	LabeledInteractor *text;
 	NSArray *keys = [touches allKeys];
 	NSNumber *uid;
+	
+	GLdouble dRadius = 0.1;
+	GLdouble dAngle;
+	
+	glTranslated(0.8 * 640, 320, 0);
+	
+	glColor3f(1, 1, 1);
+	glBegin(GL_LINE_STRIP);
+	for(dAngle = 0; dAngle <= 200; dAngle += 0.1)
+	{
+		glVertex2d(dRadius * cos (dAngle), dRadius * sin(dAngle));
+		dRadius *= 1.02;
+	}
+	glEnd();
 	
 	for(uid in keys)
 	{
 		text = [touches objectForKey:uid];
-		NSString *string = text.label;
+		const char *string = [text.label UTF8String];
+		int length = strlen(string);
 		
 		RGBA color;
 		[(NSValue*)[colors objectForKey:uid] getValue:&color];
-		
 		glColor3f(color.r, color.g, color.b);
-				glPushMatrix();
 		
+		glPushMatrix();
 		glTranslated(text.position.x, text.position.y, 0);
-		glScaled(FONT_SCALE, FONT_SCALE, 0);
 		
 		
-		glcMeasureString(GL_TRUE, [string UTF8String]);
-		glcGetStringMetric(GLC_BASELINE, baseline);
-		
-		circumference = baseline[2] - baseline[0];
-		circumference += [string length] * 0.1;
-		
+		circumference = font->Advance(string);
 		diameter = circumference / PI;
-		
-		float dX = (circumference / [string length]) / 2;
+		float dX = (circumference / length) / 2;
+
 		glTranslated(dX, diameter / 2, 0);
 		glRotated(text.angle, 0, 0, 1);
 		glTranslated(-dX, -(diameter / 2), 0);
-		
-		for(int i = 0; i < [string length]; i += 1)
+				
+		for(int i = 0; i < length; i += 1)
 		{
-			glcRenderChar([string characterAtIndex:i]);
+			font->Render(&string[i], 1);
 			
-			glTranslated(0.1 , 0, 0);
-			glRotated(360.f / [string length], 0, 0, 1);
+			glTranslated(font->Advance(&string[i], 1) , 0, 0);
+			glRotated(360.f / length, 0, 0, 1);
 		}
 		
 		glPopMatrix();
 		
 		text.angle += 1;
-	}
+	} 
 }
 
 @end
