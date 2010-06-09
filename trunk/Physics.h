37b492babee38fdb8a26bacfdfb4e9e5527bc5db
root.darkstar <root.darkstar@1bd3bf80-89e4-11de-b317-bf1d2adb44e4>
2010-06-09 10:53:47 +0000

    - Remove old project

--- trunk/Physics.h
+++
@@ -1,9 +0,0 @@
-/*
- *  Physics.h
- *  Finger
- *
- *  Created by Mood on 9/25/09.
- *  Copyright 2009 The Pixel Factory. All rights reserved.
- *
- */
-
--- trunk/messages.h
+++
@@ -1,8 +0,0 @@
-/*
- *  messages.h
- *  Finger
- *
- *  Created by Mood on 9/16/09.
- *  Copyright 2009 The Pixel Factory. All rights reserved.
- *
- */
